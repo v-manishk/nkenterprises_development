6817b1bbf3ae5c91f02615bdd113ce344cee5ae8
v-manishk <vmanish1313@gmail.com>
2023-10-15 10:38:06 +0530

    correcting the fetching data part

--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+import cors from "cors";
+import bodyParser from "body-parser";
 const app = new express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/api", (req, res) => {
     res.json({"status": 200, "created": "app created"})
