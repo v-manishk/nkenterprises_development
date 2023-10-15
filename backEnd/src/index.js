840e74e5ae533e6ec270cf163972841b5fdfad81
v-manishk <vmanish1313@gmail.com>
2023-10-15 11:50:17 +0530

    changes

--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = new express();
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(bodyParser.json());
+
 
 app.get("/api", (req, res) => {
     res.json({"status": 200, "created": "app created"})
